2fe9eb66c8933b1d4995b93c0869416f1c3ead87
Jaylon Christy <jaylon_christy@yahoo.com>
2025-05-08 21:26:27 -0500

    Creating output print statement

--- parser.c
+++ parser.c
@@ -26,12 +26,44 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-        char output[] = "output";
-
-        // output
-        if (strcmp(line, output)) {
-            printf("%s\n", line);
+        // OUTPUT (print statement)
+        char input[10];
+        for (int i = 0; i <= 6; i++) {
+            input[i] = line[i];
         }
+
+        if (strcmp("output", input)) {
+            char temp_output[100];
+            for (int i = 7; i < line[strlen(line) - 1]; i++) {
+                if (line[i] == ';') {
+                    break;
+                }
+                temp_output[i - 7] = line[i];
+            }
+
+            char temp_two_output[100];
+            char final_output[100];
+            char *ptr = &temp_output[0];
+            if (strcmp("\"", ptr)) {
+                for (int i = 1; i < temp_output[strlen(temp_output) - 1]; i++) {
+                    if (temp_output[i] == '"') {
+                        break;
+                    }
+                    temp_two_output[i - 1] = temp_output[i];
+                    for (int i = 0; i < temp_two_output[strlen(temp_two_output) - 1]; i++) {
+                        final_output[i] = temp_two_output[i];
+                    }
+                }
+                printf("%s\n", final_output);
+                /*Debugging*/
+                printf("Quote Parsed: %s\n", temp_two_output);
+                printf("Removed output: %s\n", temp_output);
+            }
+        } else {
+            printf("Error");
+        }
+
+        // This comment is here to remind you of the location of line's while loop
     }
 
     fclose(file);
--- test.c
+++ test.c
@@ -1,5 +1,11 @@
+#include <stdio.h>
+#include <string.h>
 /*
 This is the start of test.c
 Use to practice commands and functions
 */
 
+int main() {
+    printf("\"\n");
+    return 0;
+}
