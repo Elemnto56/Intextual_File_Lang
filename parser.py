1df7724ff8564d261a9199eb42864f7daa51161b
Jaylon Christy <jaylon_christy@yahoo.com>
2025-05-19 18:59:58 -0500

    Fixed parser logic

--- parser.py
+++ parser.py
@@ -21,198 +21,202 @@ def read(path):
 
 try:
     file = open(filename)
+    lines = file.readlines()
+    file_i = 0 
+    
+    while file_i < len(lines):
+        line = lines[file_i].strip()
 
-    # Storages
-    variables = {}
-    types = {}
-    craft_args = {}
-    answer = ""
+        # Storages
+        variables = {}
+        types = {}
+        craft_args = {}
+        answer = ""
 
-    # Functions (In the FIle)
-    def crunch(arg_1, arg_2, op, forced_type=None):
-        result = 0
-        if op == '+':
-            if forced_type == "string":
-                result = int(arg_1) + int(arg_2)
-                answer = str(result)
-                return answer
-            else:
-                return int(arg_1) + int(arg_2)
-        elif op == '-':
-            if forced_type == "string":
-                result = int(arg_1) - int(arg_2)
-                answer = str(result)
-                return answer
-            else:
-                return int(arg_1) - int(arg_2)
-        elif op == '*':
-            if forced_type == "string":
-                result = int(arg_1) * int(arg_2)
-                answer = str(result)
-                return answer
-            else:
-                return int(arg_1) * int(arg_2)
-        elif op == '/':
-            if forced_type == "string":
-                result = int(arg_1) / int(arg_2)
-                answer = str(result)
-                return answer
-            else:
-                return int(arg_1) / int(arg_2)
-
-    for line in file:
-        input = line.strip()
-
-        # Comment logic
-        input = input.split("//")[0].strip()
-
-        if not input:
-            continue
-
-        if input.endswith(';'):
-            if input.count("declare") != 1 and input.count("=") == 1:
-                craft_raw = input.split("=")
-            try:    
-                # DECLARE variable declaration --Integers
-                if input.startswith("int declare"):
-                    int_input = input[11:].strip().replace(';','')
-                    name, value = int_input.split("=")
-                    name = name.strip()
-                    value = value.strip()
-                    # Crunch insertion
-                    if value.count("crunch"):
-                        raw_crunch = value
-
-                        func_name = raw_crunch[:raw_crunch.index("(")]
-                        arg_string = raw_crunch[raw_crunch.index("(")+1 : -1]
-
-                        raw_args = arg_string.split(",")
-                        final_args = [argument.strip() for argument in raw_args]
-                        fixed_args = []
-                        for arg in final_args:
-                            if arg.isdigit():
-                                fixed_args.append(int(arg))
-                            elif arg in variables:
-                                fixed_args.append(variables[arg])
-                            else:
-                                fixed_args.append(arg)
-
-                        crunch_result = crunch(fixed_args[0], fixed_args[1], fixed_args[2])
-                        variables[name] = crunch_result
-                    else:
-                        variables[name] = value
-                        types[name] = "int"
-                # DECLARE --Strings
-                elif input.startswith("string declare"):
-                    raw_string = input[14:].strip().replace(';','')
-                    name, value = raw_string.split("=")
-                    name = name.strip()
-                    value = value.strip().replace('"','')
-                    # Crunch insertion
-                    if value.count("crunch"):
-                        raw_crunch = value
-
-                        func_name = raw_crunch[:raw_crunch.index("(")]
-                        arg_string = raw_crunch[raw_crunch.index("(")+1 : -1]
-
-                        raw_args = arg_string.split(",")
-                        final_args = [argument.strip() for argument in raw_args]
-                        fixed_args = []
-                        for arg in final_args:
-                            if arg.isdigit():
-                                fixed_args.append(int(arg))
-                            elif arg in variables:
-                                fixed_args.append(variables[arg])
-                            else:
-                                fixed_args.append(arg)
-                        crunch_result = crunch(fixed_args[0], fixed_args[1], fixed_args[2], fixed_args[3])
-                        variables[name] = crunch_result  
-                    # read insertion
-                    elif value.count("read"):
-                        raw_read = value
-
-                        func_name = raw_read[:raw_read.index("(")]
-                        arg_string = raw_read[raw_read.index("(")+1 : -1]
-
-                        read_result = read(arg_string)
-                        variables[name] = read_result
-                    else:
-                        variables[name] = str(value)
-                        types[name] = "string"
-                # DECLARE --Booleans
-                elif input.startswith("bool declare"):
-                    raw = input[12:].strip().replace(';','')
-                    name, value = raw.split("=")
-                    name = name.strip()
-                    value = value.strip()
-
-                    if value.lower() == "true":
-                        variables[name] = True
-                        types[name] = "bool"
-                    elif value.lower() == "false":
-                        variables[name] = False
-                        types[name] = "bool"
-                    else:
-                        print("Error: Invalid boolean")
-                # DECLARE --Floats
-                elif input.startswith("float declare"):
-                    float_input = input[13:].strip().replace(';','')
-                    name, value = float_input.split("=")
-                    name = name.strip()
-                    value = value.strip()
-                    variables[name] = float(value)
-                    types[name] = "float"
-                # DECLARE --Char
-                elif input.startswith("char declare"):
-                    raw = input[12:].strip().replace(';','')
-                    name, value = raw.split("=")
-                    name = name.strip()
-                    value = value.strip()
-                    value = ord(value)
-                    variables[name] = chr(value)
-                    types[name] = "float"
-            except (NameError, ValueError):
-                print("Error: Declaration syntax incorrect")
-
-            # OUTPUT print statement logic
-            if input[:7].count("output") == 1:
-                output_expr = input[7:].replace(';', '').strip()
-                if output_expr.count("+") >= 1:
-                    parts = output_expr.split(" + ")
-                    result = ""
-
-                    for part in parts:
-                        part = part.strip().replace('"','')
-
-                        if part in variables:
-                            result += str(variables[part])
-                        else:
-                            result += part
-                    print(result)
-                elif output_expr.startswith('"'):
-                    print(output_expr.replace('"',''))
-                # Going past standard
-                elif output_expr in variables:
-                    print(variables[output_expr])
-                elif input.count("declare") == 1:
-                    continue
-                elif output_expr == "true" or output_expr == "false":
-                    print(output_expr)
-                elif output_expr.isdigit():
-                    print(output_expr)
+        # Functions (In the File)
+        def crunch(arg_1, arg_2, op, forced_type=None):
+            result = 0
+            if op == '+':
+                if forced_type == "string":
+                    result = int(arg_1) + int(arg_2)
+                    answer = str(result)
+                    return answer
                 else:
-                    try:
-                        result = eval(output_expr, {"__builtins__": None}, variables)
-                        print(result)
-                    except Exception:
-                        print("Error: Incorrect syntax for output")
-            if input.count("=") == 1:
-                input = input.split("=")
-                if input[0].count("declare") != 1 or input[0].count("output") != 1:
-                    name = input[0].strip()
+                    return int(arg_1) + int(arg_2)
+            elif op == '-':
+                if forced_type == "string":
+                    result = int(arg_1) - int(arg_2)
+                    answer = str(result)
+                    return answer
+                else:
+                    return int(arg_1) - int(arg_2)
+            elif op == '*':
+                if forced_type == "string":
+                    result = int(arg_1) * int(arg_2)
+                    answer = str(result)
+                    return answer
+                else:
+                    return int(arg_1) * int(arg_2)
+            elif op == '/':
+                if forced_type == "string":
+                    result = int(arg_1) / int(arg_2)
+                    answer = str(result)
+                    return answer
+                else:
+                    return int(arg_1) / int(arg_2)
 
-        elif input.strip().endswith(';') != True: 
-            print("Error: A semicolon was not found in this line")
+            # Comment logic
+            line = line.split("//")[0].strip()
+
+            if not line:
+                continue
+
+            if line.endswith(';'):
+                if line.count("declare") != 1 and line.count("=") == 1:
+                    try:    
+                        # DECLARE variable declaration --Integers
+                        if line.startswith("int declare"):
+                            int_line = line[11:].strip().replace(';','')
+                            name, value = int_line.split("=")
+                            name = name.strip()
+                            value = value.strip()
+                            # Crunch insertion
+                            if value.count("crunch"):
+                                raw_crunch = value
+
+                                arg_string = raw_crunch[raw_crunch.index("(")+1 : -1]
+
+                                raw_args = arg_string.split(",")
+                                final_args = [argument.strip() for argument in raw_args]
+                                fixed_args = []
+                                for arg in final_args:
+                                    if arg.isdigit():
+                                        fixed_args.append(int(arg))
+                                    elif arg in variables:
+                                        fixed_args.append(variables[arg])
+                                    else:
+                                        fixed_args.append(arg)
+
+                                crunch_result = crunch(fixed_args[0], fixed_args[1], fixed_args[2])
+                                variables[name] = crunch_result
+                            else:
+                                variables[name] = value
+                                types[name] = "int"
+                        # DECLARE --Strings
+                        elif line.startswith("string declare"):
+                            raw_string = line[14:].strip().replace(';','')
+                            name, value = raw_string.split("=")
+                            name = name.strip()
+                            value = value.strip().replace('"','')
+                            # Crunch insertion
+                            if value.count("crunch"):
+                                raw_crunch = value
+
+                                arg_string = raw_crunch[raw_crunch.index("(")+1 : -1]
+
+                                raw_args = arg_string.split(",")
+                                final_args = [argument.strip() for argument in raw_args]
+                                fixed_args = []
+                                for arg in final_args:
+                                    if arg.isdigit():
+                                        fixed_args.append(int(arg))
+                                    elif arg in variables:
+                                        fixed_args.append(variables[arg])
+                                    else:
+                                        fixed_args.append(arg)
+                                crunch_result = crunch(fixed_args[0], fixed_args[1], fixed_args[2], fixed_args[3])
+                                variables[name] = crunch_result  
+                            # read insertion
+                            elif value.count("read"):
+                                raw_read = value
+
+                                arg_string = raw_read[raw_read.index("(")+1 : -1]
+
+                                read_result = read(arg_string)
+                                variables[name] = read_result
+                            else:
+                                variables[name] = str(value)
+                                types[name] = "string"
+                        # DECLARE --Booleans
+                        elif line.startswith("bool declare"):
+                            raw = line[12:].strip().replace(';','')
+                            name, value = raw.split("=")
+                            name = name.strip()
+                            value = value.strip()
+
+                            if value.lower() == "true":
+                                variables[name] = True
+                                types[name] = "bool"
+                            elif value.lower() == "false":
+                                variables[name] = False
+                                types[name] = "bool"
+                            else:
+                                print("Error: Invalid boolean")
+                        # DECLARE --Floats
+                        elif line.startswith("float declare"):
+                            float_line = line[13:].strip().replace(';','')
+                            name, value = float_line.split("=")
+                            name = name.strip()
+                            value = value.strip()
+                            variables[name] = float(value)
+                            types[name] = "float"
+                        # DECLARE --Char
+                        elif line.startswith("char declare"):
+                            raw = line[12:].strip().replace(';','')
+                            name, value = raw.split("=")
+                            name = name.strip()
+                            value = value.strip()
+                            value = ord(value)
+                            variables[name] = chr(value)
+                            types[name] = "float"
+                    except (NameError, ValueError):
+                        print("Error: Declaration syntax incorrect")
+
+                # REPEAT while loops
+                if line.startswith("repeat"):
+                    lines = []
+                    lines.append(line)
+
+
+
+                # OUTPUT print statement logic
+                if line.startswith("output"):
+                    output_expr = line[7:].replace(';', '').strip()
+                    if output_expr.count("+") >= 1:
+                        parts = output_expr.split(" + ")
+                        result = ""
+
+                        for part in parts:
+                            part = part.strip().replace('"','')
+
+                            if part in variables:
+                                result += str(variables[part])
+                            else:
+                                result += part
+                        print(result)
+                    elif output_expr.startswith('"'):
+                        print(output_expr.replace('"',''))
+                    # Going past standard
+                    elif output_expr in variables:
+                        print(variables[output_expr])
+                    elif line.count("declare") == 1:
+                        continue
+                    elif output_expr == "true" or output_expr == "false":
+                        print(output_expr)
+                    elif output_expr.isdigit():
+                        print(output_expr)
+                    else:
+                        try:
+                            result = eval(output_expr, {"__builtins__": None}, variables)
+                            print(result)
+                        except Exception:
+                            print("Error: Incorrect syntax for output")
+
+            elif line.strip().endswith(';') != True: 
+                if line.endswith('{') or line.endswith('}'):
+                    continue
+                else:
+                    print("Error: A semicolon was not found in this line")
 
 except FileNotFoundError:
     print("Error: Intext file not found")
--- test.py
+++ test.py
@@ -1,9 +1,16 @@
-def read(path):
-    try:
-        with open(path, 'r') as f:
-            return f.read()
-    except Exception as e:
-        print(f"Error: Couldn't read file '{path}'")
-        return None
-    
-print(read("text.txt"))
+file = open("main.itx")
+
+for line in file:
+    input = line.strip()
+    if input.startswith("repeat"):
+        logic = input[6:input.index('{')].strip()
+        print(logic)
+        print("Starts with repeat")
+        if logic.count("true") == 1:
+            print("Saw true")
+            if input.startswith('}'):
+                closing_repeat = input
+                for thing in input[input.index('{')+1:closing_repeat]:
+                    raw_lines = []
+                    raw_lines.append(thing)
+                    print(raw_lines)
